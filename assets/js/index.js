0358619b405c95d0282dba249ad9f85e897ca0ce
untamed-network <untamed.spprt@gmail.com>
2025-12-28 10:40:20 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -109,7 +109,7 @@ const fetchLeaderboard = async () => {
 
 const fetchBotInfo = async () => {
     try {
-        const response = await fetch('https://212.227.166.131:10185/api/stats');
+        const response = await fetch('https://api.useless.shapes.lol/api/stats');
         const data = await response.json();
         
         const usersEl = document.getElementById('users');
